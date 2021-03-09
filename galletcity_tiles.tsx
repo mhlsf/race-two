eff747e5b37601dd6ec59e8d9f286a959b056bf4
Mahel SIf <m.sif@thecodingmachine.com>
2021-03-09 11:38:47 +0100

    Add map

---
+++ galletcity_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="galletcity_tiles" tilewidth="32" tileheight="32" tilecount="168" columns="8">
+ <image source="galletcity_tiles.png" width="256" height="672"/>
+</tileset>
